2f079a20479fb94f4eb724e49e102d5ef1cd456c
Annie40 <73389222+Annie40@users.noreply.github.com>
2020-11-07 17:45:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var movingRect, fixedRect;
+var rect1, rect2;
+
+function setup() {
+  createCanvas(1200,400);
+
+  fixedRect = createSprite(200, 200, 50, 80);
+  fixedRect.shapeColor = "green";
+
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+
+  rect1 = createSprite(400,200,50,50);
+  rect1.shapeColor = "blue";
+  rect1.velocityY = 3;
+
+  rect2 = createSprite(400,300,50,50);
+  rect2.shapeColor = "blue";
+  rect2.velocityY = -3;
+}
+
+function draw() {
+  background("black");  
+
+  movingRect.x = mouseX;
+  movingRect.y = mouseY;
+
+  if(collide(movingRect, fixedRect))
+  {
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
+  }
+  else
+  {
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
+  }
+
+  bounceOff(rect1, rect2);
+  
+  drawSprites(); 
+}
